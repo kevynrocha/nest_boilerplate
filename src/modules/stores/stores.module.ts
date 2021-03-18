2f1676efa473636e5d9e9429b70b319a7d112eb7
Kevyn Rocha <kevyn_oliver@hotmail.com>
2021-03-17 23:52:11 -0300

    teste http

--- src/modules/stores/stores.module.ts
+++ src/modules/stores/stores.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { StoresService } from './stores.service';
 import { StoresController } from './stores.controller';
 
 @Module({
+  imports: [HttpModule],
   controllers: [StoresController],
-  providers: [StoresService]
+  providers: [StoresService],
 })
 export class StoresModule {}
--- src/modules/stores/stores.service.ts
+++ src/modules/stores/stores.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { CreateStoreDto } from './dto/create-store.dto';
 import { UpdateStoreDto } from './dto/update-store.dto';
 
 @Injectable()
 export class StoresService {
+  constructor(private httpService: HttpService) {}
+
   create(createStoreDto: CreateStoreDto) {
     return 'This action adds a new store';
   }
 
-  findAll() {
-    return `This action returns all stores`;
+  async findAll(): Promise<any> {
+    const response = await this.httpService
+      .get('https://brasilapi.com.br/api/cep/v1/79084212')
+      .toPromise();
+
+    return response.data;
   }
 
   findOne(id: number) {
